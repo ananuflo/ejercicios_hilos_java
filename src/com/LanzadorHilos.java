54cd9d22c269bf413e5b2ee3a1065a803841a234
Ana Núñez <nunez.flores.ana@iescamas.es>
2025-11-07 20:26:48 +0100

    Subido ejemplo waiting

--- src/com/LanzadorHilos.java
+++ src/com/LanzadorHilos.java
@@ -5,25 +5,23 @@ public class LanzadorHilos {
 	private static final Object LOCK = new Object();
 	
 	public static void main(String[] args) {
+		
 		Thread t = new Thread(() -> {
-			
-            try {
-                // El hilo se bloquea en TIMED_WAITING durante 2 s.
-                Thread.sleep(2000);
-            } catch (InterruptedException ignored) { }
-        }, "tiempo");
-
-        t.start();                     // h comienza a ejecutarse
+            synchronized (LOCK) {
+                try {
+                    System.out.println(Thread.currentThread().getName() + " esperando...");
+                    LOCK.wait();
+                } catch (InterruptedException ignored) {}
+            }
+        }, "tWaiting");
+        t.start();
         try {
-			Thread.sleep(50);
+			Thread.sleep(100);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}              // le damos tiempo para entrar en sleep
-        //Thread.sleep(10);  // Muy poco tiempo, muy probable que todavía esté en sleep
-        System.out.println(t.getName() + " -> " + t.getState()); // TIMED_WAITING o RUNNABLE según el timing
+		}
+        System.out.println(t.getName() + " -> " + t.getState());
     }
-
-
 }
 
