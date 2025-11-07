e7cf42044f98da0ee08267e791f5160bee82ec8e
Ana Núñez <nunez.flores.ana@iescamas.es>
2025-11-07 20:10:36 +0100

    Subido ejemplo blocked

--- src/com/LanzadorHilosNew.java
+++ src/com/LanzadorHilosNew.java
@@ -2,20 +2,38 @@ package com;
 
 public class LanzadorHilosNew {
 
+	private static final Object LOCK = new Object();
+	
 	public static void main(String[] args) {
 
-		Thread h = new Thread(new Hilo("runnable"));
-		h.start();
+		Thread t1 = new Thread(() -> {
+            synchronized (LOCK) {
+                try { Thread.sleep(2000); } catch (InterruptedException ignored) {}
+            }
+        }, "t1");
 		
-		try {
-			
-			Thread.sleep(50);
-			System.out.println(h.getName() + " ---> " + h.getState());
-
-			
-		}catch(Exception e) {
+		Thread t2 = new Thread(() -> {
+	           synchronized (LOCK) {
+	               System.out.println("Bloqueado: " + Thread.currentThread().getName());
+	            }
+	        }, "t2");
+		
+		t1.start();
+        try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+        t2.start();
+        try {
+			Thread.sleep(50);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        System.out.println(t2.getName() + " -> " + t2.getState());
+		
 	}
 
 }
