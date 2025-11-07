7033c7fc6a3d1a46cf368dfafe364d4f2d7231d7
Ana Núñez <nunez.flores.ana@iescamas.es>
2025-11-07 19:57:55 +0100

    Subido el estado runnable

--- src/com/LanzadorHilosNew.java
+++ src/com/LanzadorHilosNew.java
@@ -4,8 +4,18 @@ public class LanzadorHilosNew {
 
 	public static void main(String[] args) {
 
-		Thread h = new Thread(new Hilo("nuevo"));
-		System.out.println(h.getName() + " -> " + h.getState());
+		Thread h = new Thread(new Hilo("runnable"));
+		h.start();
+		
+		try {
+			
+			Thread.sleep(50);
+			System.out.println(h.getName() + " ---> " + h.getState());
+
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
